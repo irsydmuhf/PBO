6ae13f205098220669c945acc8dcb4406ecad574
irsydmuhf <151486551+irsydmuhf@users.noreply.github.com>
2024-05-22 21:45:21 +0700

    Update Grandparent.java

--- Main/src/main/Grandparent.java
+++ Main/src/main/Grandparent.java
@@ -1,19 +1,1 @@
-package main;
 
-/**
- *
- * @author DELL
- */
-public class Grandparent extends Keluarga {
-    public Grandparent(String nama, String jenisKelamin, int usia) {
-        super(nama, jenisKelamin, usia);
-    }
-
-    @Override
-    public void tampilkanInfo() {
-        System.out.println("Nama: " + nama);
-        System.out.println("Jenis Kelamin: " + jenisKelamin);
-        System.out.println("Usia: " + usia);
-        System.out.println("Jabatan: Grandparent");
-    }
-}
